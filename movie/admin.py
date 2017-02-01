022de680f1cd54ca29cf87ca0b7e636138f55fef
ciazhar <daisetsunakama@gmail.com>
2017-02-01 11:47:56 +0700

    menambahkan fungsi untuk menampilkan di admin

--- movie/admin.py
+++ movie/admin.py
@@ -6,7 +6,17 @@ class  GenreAdmin(admin.ModelAdmin):
     pass
 
 class  MovieAdmin(admin.ModelAdmin):
-    list_display = {'title', 'posted_by', 'show_from', 'show_until', 'created_at'}
+    #yang ditampilkan di movie
+    list_display = ('title','show_genres', 'posted_by', 'show_from', 'show_until', 'created_at', 'show_status')
 
-    admin.site.register(Genre, GenreAdmin)
-    admin.site.register(Movie, GenreAdmin)
+    #yang ditampilan di form
+    fields = ('title','description', 'show_from', 'show_until', 'genres')
+
+    #otomatisasi save posted by
+    def save_model(self, request, obj, form, change):
+        obj.posted_by = request.user
+        super(MovieAdmin, self).save_model(request,obj,form, change)
+
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Movie, MovieAdmin)
--- movie/models.py
+++ movie/models.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
-
+from datetime import datetime
 # Create your models here.
 
 class  Genre(models.Model):
@@ -19,14 +19,14 @@ class  Genre(models.Model):
 
 
 class  Movie(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
+    title = models.CharField(max_length=200, verbose_name="judul") #verbose_name digunakaan untuk mengubah nama di UI
+    description = models.TextField("deskripsi",null=True, blank=True)#mengubah nama di UI juga bisa di parameter pertama
     show_from = models.DateField()
     show_until = models.DateField()
     genres = models.ManyToManyField(Genre)
     posted_by = models.ForeignKey(User)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    created_at = models.DateField(auto_now_add=True)#ketika kita membuat pertama kali akan dibuat pawa waktu pertama kali, ketika ada baru gk update
+    updated_at = models.DateField(auto_now=True)#ketika membuat pertambahan maka akan diupdate
 
     #toString pyton 3
     def __str__(self):
@@ -34,3 +34,27 @@ class  Movie(models.Model):
     #toString pyton 2
     def __unicode__(self):
         return self.title
+    #fungsi Menampilkan macam-macam genre
+    def show_genres(self):
+        genre = self.genres.all()
+        text=''
+        for data in genre:
+            text += data.title+", "
+        return text
+
+    def in_show(self):
+        now = datetime.now().date()
+        if now >= self.show_from and now <= self.show_until:
+            return 1 #sedang tayang
+        elif now > self.show_until:
+            return -1 #sudah tayang
+        else :
+            return 0 #segera tayang
+
+    def show_status(self):
+        if self.in_show() == 1 :
+            return "sedang tayang"
+        elif self.in_show()== -1 :
+            return "selesai tayang"
+        else :
+            return "segera tayang"
